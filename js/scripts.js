0efdf441ad2d4cd277856645b5e9ca411bd5f807
Connor Hansen <chansen13@georgefox.edu>
2021-08-26 12:00:22 -0700

    add score counter, win condition, and reset button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-function Player(name, score, turnScore, isTurn) {
+function Player(name, score, turnScore) {
   this.name = name;
   this.score = score;
   this.turnScore = turnScore;
-  this.isTurn = isTurn;
 }
 
 Player.prototype.rollDice = function () {
   tempScore = Math.floor(Math.random() * 6 + 1);
   console.log("Roll score: " + tempScore);
   if (tempScore === 1) {
-    this.turnScore += 0;
+    this.turnScore = 0;
     if (turnCounter === 1) {
       turnCounter = 2;
     } else if (turnCounter === 2) {
@@ -19,6 +18,7 @@ Player.prototype.rollDice = function () {
     this.turnScore += tempScore;
   }
 }
+
 
 Player.prototype.hold = function () {
   this.score += this.turnScore;
@@ -31,9 +31,76 @@ Player.prototype.hold = function () {
 }
 
 
-player1 = new Player("player1", 0, 0, 1);
-player2 = new Player("player2", 0, 0, 2);
+player1 = new Player("player1", 0, 0);
+player2 = new Player("player2", 0, 0);
+// var tempScore = 0;
 var turnCounter = 1;
+
+
+// function updateDisables(player) {
+//   if (player.id == 1) {
+//       $("#playerTwoHold").prop("disabled", false);
+//       $("#playerOneHold").prop("disabled", true);
+//       $("#playerTwoRoll").prop("disabled", false);
+//       $("#playerOneRoll").prop("disabled", true);
+//   } else if (player.id == 2) {
+//       $("#playerTwoHold").prop("disabled", true);
+//       $("#playerOneHold").prop("disabled", false);
+//       $("#playerTwoRoll").prop("disabled", true);
+//       $("#playerOneRoll").prop("disabled", false);
+//   }
+// }
+
+// function()
+
+// var x = tempScore;
+
+// if (x = 1) {
+//           d.innerHTML = '<img src=https://image.ibb.co/cQKOhc/dice1.png>';
+//   }
+
+$(document).ready(function () {
+  $("#playerRoll").click(function () {
+    console.log("Turn counter: " + turnCounter);
+    if (turnCounter === 1) {
+      
+      player1.rollDice();
+    } else if (turnCounter === 2) {
+      
+      player2.rollDice();
+    }
+    $("#player1Score").html(player1.score);
+    $("#player1TurnScore").html(player1.turnScore);
+    $("#player2Score").html(player2.score);
+    $("#player2TurnScore").html(player2.turnScore);
+  });
+
+  $("#playerHold").click(function () {
+    console.log("Turn counter: " + turnCounter);
+    if (turnCounter === 1){
+    player1.hold();
+    $("#player1TurnScore").html(0);
+    if (player1.score >= 20) {
+      $("#playerHold,#playerRoll").hide();
+      $("#player1Win").show();
+    }
+    console.log("P1 Score: " + player1.score);
+    } else if (turnCounter === 2) {
+    player2.hold();
+    $("#player2TurnScore").html(0);
+      if (player2.score >= 20) {
+        $("#playerHold,#playerRoll").hide();
+        $("#player2Win").show();
+      }
+      console.log("P2 Score: " + player2.score);
+    }
+    $("#player1Score").html(player1.score);
+    $("#player2Score").html(player2.score);
+    $(".btn-warning").click(function () {
+      location.reload();
+    });
+  });
+});
 
 // $("button#playerOneHold").click(function) {
 //     const result = playerOne.roll();
@@ -52,61 +119,6 @@ var turnCounter = 1;
 // function updatePlayerOneOverallTotal(player) {
 //     $$("playerOneOverallTotal").text(player.pointTotal);
 // }
-
-
-$(document).ready(function () {
-  $("#playerRoll").click(function () {
-    console.log("Turn counter: " + turnCounter);
-    if (turnCounter === 1) {
-      player1.rollDice();
-    } else if (turnCounter === 2) {
-      player2.rollDice();
-    }
-    $("#player1Score").html(player1.score);
-    $("#player2Score").html(player2.score);
-  });
-
-  $("#playerHold").click(function () {
-    console.log("Turn counter: " + turnCounter);
-    if (turnCounter === 1){
-    player1.hold();
-    console.log("P1 Score: " + player1.score);
-    } else if (turnCounter === 2) {
-    player2.hold();
-    console.log("P2 Score: " + player2.score);
-    }
-    $("#player1Score").html(player1.score);
-    $("#player2Score").html(player2.score);
-  });
-});
-
-
-
-
-
-// while player's turn = true
-//   two buttons: rollDice, hold
-//   on click for rollDice
-//     rollDice
-//     if rollDice === 1 && player1
-//       set player2.isturn to true
-//     else if rollDice === 1 && player2
-//       set player1.isturn to true
-
-//   on click for hold
-//     if player1, set player2.isTurn to true
-//     if player2, set player1.isTurn to true
-
-// while turncounter = 1
-//   two buttons: rollDice, hold
-//   on click for rollDice
-//     rollDice
-//     if rollDice === 1, turncounter = 2
-// while turncounter = 2
-//   two buttons: rollDice, hold
-//   on click for rollDice
-//     rollDice
-//     if rollDice === 1, turncounter = 1
 
 
 // Player.prototype.hold = function() {
